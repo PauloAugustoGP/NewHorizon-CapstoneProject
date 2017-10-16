d45b6f4d52d4d42bdefe581f11bcab9785ef4966
Brian Serralheiro <brian.serralheiro@gmail.com>
2017-10-16 09:55:31 -0400

    SwitchButton(Working)

--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/switchButton.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/switchButton.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class switchButton : MonoBehaviour {
+public class switchButton : MonoBehaviour
+{
+    //Audio
+    public AudioClip OpenedSFX;
 
-    public bool activated;
+    public AudioSource audioSource;
 
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        if (activated)
+        audioSource = GetComponent<AudioSource>();
+        if (!audioSource)
         {
-
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+            audioSource.loop = false;
+            audioSource.volume = 1.0f;
         }
     }
 	
@@ -21,5 +27,28 @@ public class switchButton : MonoBehaviour {
     {
 		
 	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.other.CompareTag("Projectile"))
+        {
+            playSound(OpenedSFX);
+            Debug.Log("Door Opened!");
+        }
+
+    }
+
+    // Function used to set clip to play through AudioSource attached to Character
+    void playSound(AudioClip clip, float volume = 1.0f)
+    {
+        //Assign volume to AudioSource volume
+        audioSource.volume = volume;
+
+        // Tell AudioSource what clip to play
+        audioSource.clip = clip;
+
+        // Tell AudioSource to play sound
+        audioSource.Play();
+    }
 
 }
---
+++ CapstoneProject/TFS-CapstoneProject/Assets/Brian Folder/Script/brianProjectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brianProjectile : MonoBehaviour
+{
+
+    public Rigidbody projectile;
+    public Transform projectileSpawnPoint;
+    public float projectileSpeed = 10.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Pew Pew");
+
+            if (projectile)
+            {
+                Rigidbody rb = Instantiate(projectile, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+
+                rb.AddForce(projectileSpawnPoint.forward * projectileSpeed, ForceMode.Impulse);
+            }
+            else
+                Debug.Log("No projectile prefab found.");
+        }
+    }
+}
