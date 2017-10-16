e232e3c435077c2f31878c9648891529cf3b91fc
STUDENTS-STL\Garrett.May <maygarrett@hotmail.com>
2017-10-16 10:28:33 -0400

    Changes to teleport: moved many files around in my folder. Added new teleport scripts for teleport character, teleport point, raduis indicator, added prefabs for fake player and radius indicator. To add teleport to a player must add the teleportscript to player, teleport point to scene and assign prefabs in the teleportscript in the inspector.

---
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Garrett_Teleport/Scripts/TeleportScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+
+    public GameObject fakePlayerPrefab;
+    public GameObject radiusPrefab;
+
+    private GameObject fakePlayer;
+    GameObject teleportPoint;
+    private GameObject radius;
+
+    Vector3 fakePlayerSpawnLocation;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        teleportPoint = GameObject.FindGameObjectWithTag("TeleportLocation");
+        if(!teleportPoint)
+        {
+            Debug.Log("No Teleport Point Prefab added to scene.");
+        }
+
+        fakePlayerSpawnLocation = new Vector3(0, 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+
+            
+
+            Instantiate(fakePlayerPrefab, teleportPoint.transform.position + fakePlayerSpawnLocation, teleportPoint.transform.rotation);
+
+            fakePlayer = GameObject.FindGameObjectWithTag("FakePlayer");
+
+            radius = Instantiate(radiusPrefab, transform.position, radiusPrefab.transform.rotation);
+                
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+
+            Destroy(fakePlayer);
+            Destroy(radius);
+
+            if (Vector3.Distance(transform.position, teleportPoint.transform.position) < 5 && Vector3.Distance(transform.position, teleportPoint.transform.position) > 1)
+            {
+                transform.SetPositionAndRotation(teleportPoint.transform.position, transform.rotation);
+            }
+        }
+
+
+    }
+}
