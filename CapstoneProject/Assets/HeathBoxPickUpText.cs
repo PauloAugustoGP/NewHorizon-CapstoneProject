d05d6887b96dd9a283aa045b7b76d5d53c8b0664
MSI\drema <Dremand@hotmail.com>
2017-10-16 13:40:11 -0400

    Made a new scene in Andre folder, put a health box in with a trigger to make a text pop up and say you have picked up a med kit.

---
+++ CapstoneProject/Assets/HeathBoxPickUpText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeathBoxPickUpText : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+
+            GameObject gm = GameObject.FindGameObjectWithTag("MainCamera");
+            if (gm)
+            {
+                PlayerUI pm = gm.GetComponent<PlayerUI>();
+                if (pm)
+                {
+                    pm.SpawnMessage("You picked up a med kit.");
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider c)
+    {
+        Destroy(gameObject);
+    }
+}
