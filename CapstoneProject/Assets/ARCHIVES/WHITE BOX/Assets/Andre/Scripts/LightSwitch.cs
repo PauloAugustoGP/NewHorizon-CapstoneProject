351d6220d83207332d5747b3d3d2c16b2637b3a7
MSI\drema <Dremand@hotmail.com>
2017-10-17 11:12:49 -0400

    Put celing lights in small scene in Andre folder, added more trigger boxes for Instructions when walking into Lab 1.

---
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Andre/Scripts/LightSwitch.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour {
+
+    //Audio
+    public AudioClip OpenedSFX;
+
+    public AudioSourcaaaaaae audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (!audioSource)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+            audioSource.loop = false;
+            audioSource.volume = 1.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.other.CompareTag("Projectile"))
+        {
+            playSound(OpenedSFX);
+            Debug.Log("Door Opened!");
+        }
+
+    }
+
+    // Function used to set clip to play through AudioSource attached to Character
+    void playSound(AudioClip clip, float volume = 1.0f)
+    {
+        //Assign volume to AudioSource volume
+        audioSource.volume = volume;
+
+        // Tell AudioSource what clip to play
+        audioSource.clip = clip;
+
+        // Tell AudioSource to play sound
+        audioSource.Play();
+    }
+
+}
+
