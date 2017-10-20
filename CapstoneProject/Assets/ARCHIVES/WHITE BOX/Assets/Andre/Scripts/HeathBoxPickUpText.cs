9babf8f9e402e50b7bdbf3665e196f587892796c
MSI\drema <Dremand@hotmail.com>
2017-10-20 10:31:07 -0400

    Working on setting up a text to show up in the scene when hitting the health box, instead of the UI.

--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Andre/Scripts/HeathBoxPickUpText.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Andre/Scripts/HeathBoxPickUpText.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeathBoxPickUpText : MonoBehaviour {
-
+    public RectTransform PickUpHealthSpawnPoint;
+    public PlayerMessage spawn;
     private void OnTriggerEnter(Collider c)
     {
+
         if (c.gameObject.tag == "Player")
         {
-
-            GameObject gm = GameObject.FindGameObjectWithTag("MainCamera");
-            if (gm)
-            {
-                PlayerUI pm = gm.GetComponent<PlayerUI>();
-                if (pm)
-                {
-                    pm.SpawnMessage("You picked up a med kit.");
-                }
-            }
+                    SpawnMessage("You picked up a med kit.");
         }
     }
     private void OnTriggerExit(Collider c)
     {
         Destroy(gameObject);
+    }
+    public void SpawnMessage(string message)
+    {
+        PlayerMessage pm = Instantiate(spawn);
+        pm.transform.SetParent(PickUpHealthSpawnPoint.transform);
+        pm.text.text = message;
+        
     }
 }
