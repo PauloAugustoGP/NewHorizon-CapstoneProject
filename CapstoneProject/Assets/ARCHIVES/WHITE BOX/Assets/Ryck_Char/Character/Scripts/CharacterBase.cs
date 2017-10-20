6538f928065c02eaede5b3fae7d2692f27b28f11
Ryck Griffiths <ryckgriff@gmail.com>
2017-10-20 10:54:25 -0400

    Changed Control script is now called * "CharacterBehaviour" *
    Inheritance BASE class is now called "CharacterBase"
    
    Class functions handle movement values for player
    
    **NOTE** Q and E controls for strafing are implemented not fully functional
    **NOTE** Bit of sliding due to velocity
    
    Class holds Health, TeleportResource, Damage function, RecoveryRate function
    
    Code is structured for Final setup
    
    Code Version 6.1.0 ~ Ryck G.

---
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Ryck_Char/Character/Scripts/CharacterBase.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+    protected static bool isAlive;
+
+    protected static int _health;
+    protected static int _mana;
+
+    protected static float _MoveV;
+
+    protected static bool moving;
+    protected static bool Rested;
+    protected static bool slowed;
+
+    protected float Clockwise = 500.0f;
+    protected float CounterClockwise = -500.0f;
+
+    protected Rigidbody rb;
+
+    protected void Forward()
+    {
+        moving = true;
+        rb.velocity = transform.forward * Time.deltaTime * MoveV;
+    }
+
+    protected void Backward()
+    {
+        moving = true;
+        rb.velocity = -transform.forward * Time.deltaTime * MoveV;
+    }
+
+    protected void RotateRight()
+    {
+        moving = true;
+        transform.Rotate(0, Time.deltaTime * Clockwise, 0);
+    }
+
+    protected void RotateLeft()
+    {
+        moving = true;
+        transform.Rotate(0, Time.deltaTime * CounterClockwise, 0);
+    }
+
+    protected void StrafeRight()
+    {
+        moving = true;
+        rb.velocity = transform.right * Time.deltaTime * (MoveV / 2);
+    }
+
+    protected void StrafeLeft()
+    {
+        moving = true;
+        rb.velocity = -transform.right * Time.deltaTime * (MoveV / 2);
+    }
+
+    public static int Health
+    {
+        get { return _health; }
+        set
+        {
+            _health = value;
+
+            if (_health > 100)
+            {
+                _health = 100;
+            }
+
+            if (_health < 0)
+            {
+                _health = 0;
+            }
+        }//set
+    }//health
+
+    public static int TeleportResource
+    {
+        get { return _mana; }
+        set
+        {
+            _mana = value;
+
+            if (_mana > 100)
+            {
+                _mana = 100;
+            }
+
+            if (_mana < 0)
+            {
+                _mana = 0;
+            }
+        }//set
+    }//Mana
+
+    public static float MoveV
+    {
+        get { return _MoveV; }
+        set
+        {
+            _MoveV = value;
+    
+        }//set
+    }//MoveV
+
+    public void Damage(int value)
+    {
+        Health -= value;
+    }
+
+    protected void RecoveryRate(int value)
+    {
+        if (Rested)
+        {
+            TeleportResource += value * 4;
+        }
+
+        if (!Rested)
+        {
+            TeleportResource += value;
+        }
+    }
+}
---
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Ryck_Char/Character/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CharacterBehaviour : CharacterBase
+{
+    protected int Lives;
+
+    public bool isCrouching;
+
+    Animator anim;
+
+    /// <summary>
+    /// Programmer Var
+    /// 
+
+    public bool ValueDebuger;
+
+    /// </summary>
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+        Health = 100;
+        TeleportResource = 1;
+
+        isAlive = true;
+
+        
+
+        if (Lives == 0)
+            Lives = 3;
+	}
+	
+	void Update ()
+    {
+        //Upon 0 or Low Health
+        if (Health <= 0)
+        {
+            //SoundManager.instance.SoundCaller("Death", 0.2f);
+            Died();
+        }
+
+        if (TeleportResource < 100)
+        {
+            RecoveryRate(1);
+        }
+
+        ///Temp Spot //wanted to move to class
+        if (moving)
+        {
+            slowed = isCrouching;
+
+            if (!slowed)
+            {
+                _MoveV = 180;
+            }
+
+            if (slowed)
+            {
+                _MoveV = 100;
+            }
+        }
+
+        else if (!moving)
+        {
+            _MoveV = 0;
+        }
+
+        if (isAlive)
+        {
+            //Forward
+            if ((Input.GetKey(KeyCode.W)))
+            {
+                anim.SetFloat("Speed", MoveV); 
+                Forward();
+            }
+
+            else if ((Input.GetKeyUp(KeyCode.W)))
+            {
+                moving = false;
+                anim.SetFloat("Speed", 0);
+            }
+
+            //Back
+            if ((Input.GetKey(KeyCode.S)))
+            {
+                anim.SetFloat("Speed", -MoveV);
+                Backward();
+            }
+
+            else if ((Input.GetKeyUp(KeyCode.S)))
+            {
+                moving = false;
+                anim.SetFloat("Speed", 0);
+            }
+
+            //strafeRight
+            if ((Input.GetKey(KeyCode.E)))
+            {
+                anim.SetFloat("Speed", MoveV);
+                StrafeRight();
+            }
+
+            else if ((Input.GetKeyUp(KeyCode.E)))
+            {
+                moving = false;
+                anim.SetFloat("Speed", 0);
+            }
+
+            //strafeLeft
+            if ((Input.GetKey(KeyCode.Q)))
+            {
+                anim.SetFloat("Speed", -MoveV);
+                StrafeLeft();
+            }
+
+            else if ((Input.GetKeyUp(KeyCode.Q)))
+            {
+                moving = false;
+                anim.SetFloat("Speed", 0);
+            }
+
+            //Turning
+            if (Input.GetKey(KeyCode.D))
+            {
+                RotateRight();
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                RotateLeft();
+            }
+
+            //Crouching
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                isCrouching = !isCrouching;
+
+                if (!isCrouching)
+                {
+                    anim.SetBool("Crouching", isCrouching);
+                }
+                if (isCrouching)
+                {
+                    anim.SetBool("Crouching", isCrouching);
+                }
+            }
+        }
+
+        if (ValueDebuger)
+        {
+            Debug.Log(MoveV);
+            Debug.Log(slowed);
+            Debug.Log(Health);
+            Debug.Log(TeleportResource);
+        }
+
+    }//Update
+
+    //On Death call
+    protected void Died()
+    {
+        Lives--;
+
+        if (Lives == 0)
+        {
+            SceneManager.LoadScene("lose");
+        }
+        else
+        {
+            //Load current level
+            //SceneManager.LoadScene(******);
+        }     
+    }
+}
