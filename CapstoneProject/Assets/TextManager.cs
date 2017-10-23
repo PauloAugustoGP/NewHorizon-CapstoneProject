719606a139a14986f476919b97ddbbb316ebcaf4
MSI\drema <Dremand@hotmail.com>
2017-10-23 12:50:34 -0400

    Working on text boxes with animations to appear in Andre folder SmallTestScene

---
+++ CapstoneProject/Assets/TextManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Text))]
+
+public class TextManager : MonoBehaviour {
+
+    private Typing typer;
+    private Animator menuAnim;
+    private bool menuOn = false;
+
+    private void Awake()
+    {
+        typer = GetComponentInChildren<Typing>();
+        menuAnim = GetComponent<Animator>();
+    }
+
+    public void BeginTheMenu()
+    {
+        if(!menuOn)
+        {
+            menuAnim.SetTrigger("FadeIn");
+            typer.StartCoroutine("TypeIn");
+            menuOn = true;
+        }
+        else
+        {
+            menuAnim.SetTrigger("FadeOut");
+            typer.StartCoroutine("TypeOff");
+            menuOn = false;
+        }
+    }
+}
