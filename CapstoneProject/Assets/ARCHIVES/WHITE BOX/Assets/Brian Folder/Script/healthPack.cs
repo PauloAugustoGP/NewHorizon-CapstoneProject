44b4dbeea2073199da0987bfefe47221957bec5a
Brian Serralheiro <brian.serralheiro@gmail.com>
2017-11-06 10:56:45 -0500

    HealthPack(Working).

--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/healthPack.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/healthPack.cs
@@ -4,29 +4,16 @@ using UnityEngine;
 
 public class HealthPack : MonoBehaviour
 {
-    public int Health;
 
-    void OnTriggerEnter(Collider other)
-    {
-            if (other.GetComponent<CharacterBehaviour>().CompareTag("Player"))
-            {
-                Health += 50;
-                Destroy(gameObject);
-            }
-
-        /*
         void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<CharacterBehaviour>())
-            {
-                 Change the Damage health function to public and create a heal function so I can add health to the player.
-                other.GetComponent<CharacterBehaviour>().heal(50);
-
-
+            {                 
+                other.GetComponent<CharacterBehaviour>().Heal(50);
+                Destroy(gameObject);
             }
         }
-        */
-    }
+        
 }
 
 
