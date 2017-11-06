c0c65bd695a03fff29e3ca3b0af83aebd0965bcc
Brian Serralheiro <brian.serralheiro@gmail.com>
2017-11-06 10:31:04 -0500

    SwitchButton(Cleaned, soon will be opening the door) + HealthPack(On Hold)

--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/healthPack.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/healthPack.cs
@@ -6,27 +6,27 @@ public class HealthPack : MonoBehaviour
 {
     public int Health;
 
-    void Start()
-    {
-        //Health = CharacterBehaviour.Health;
-    }
-    //I need to figure it out a Way to add health to the player's health without using the get component script within the player.
     void OnTriggerEnter(Collider other)
     {
-///<<<<<<< HEAD
-        if (other.CompareTag("Player"))
+            if (other.GetComponent<CharacterBehaviour>().CompareTag("Player"))
+            {
+                Health += 50;
+                Destroy(gameObject);
+            }
+
+        /*
+        void OnTriggerEnter(Collider other)
         {
-            Health += 3;
-///=======
-       /* if (other.GetComponent<charControlScript>())
-        {
-            //charControlScript c = other.GetComponent<charControlScript>();
-            c.PlayerHealth += 3;
->>>>>>> 97e934e2fc3dd959d58c94232f83d4d911b98bcb
-            Destroy(gameObject);
+            if (other.GetComponent<CharacterBehaviour>())
+            {
+                 Change the Damage health function to public and create a heal function so I can add health to the player.
+                other.GetComponent<CharacterBehaviour>().heal(50);
+
+
+            }
         }
         */
-		}
-	}
+    }
 }
-	
+
+
--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/switchButton.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/switchButton.cs
@@ -4,49 +4,14 @@ using UnityEngine;
 
 public class SwitchButton : MonoBehaviour
 {
-
-    [SerializeField]
-    private AudioClip OpenedSFX;
-    [SerializeField]
-    private AudioSource audioSource;
-
-    // Use this for initialization
-    void Start ()
-    {
-        audioSource = GetComponent<AudioSource>();
-        if (!audioSource)
-        {
-            audioSource = gameObject.AddComponent<AudioSource>();
-        }
-    }
-    
-
-    
-    
-    
         void OnCollisionEnter(Collision c)
     {
         if (c.gameObject.GetComponent<Projectile_ObjectScript>())
         {
-            //PlaySound(OpenedSFX);
             Debug.Log("Door Opened!");
         }
-
             
     }
-    
-
-
-    void PlaySound(AudioClip clip, float volume = 1.0f)
-    {
-        //Assign volume to AudioSource volume
-        audioSource.volume = volume;
-
-        // Tell AudioSource what clip to play
-        audioSource.clip = clip;
-
-        // Tell AudioSource to play sound
-        audioSource.Play();
-    }
+   
 
 }
