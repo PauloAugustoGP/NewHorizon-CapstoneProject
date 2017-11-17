80b6b09c11a37482740fc0f3ed99841a4e75abbc
dan.browning <browning.dan@gmail.com>
2017-11-17 10:29:43 -0500

    Adding a winning condition and a winning Scene that gives options to the player. Reoptimized the menu by taking away serializedfield's

---
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Dan's Folder/WinTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0.5f, 0.5f, 0.5f);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Scene_Manager.instance.GoTo_LEVEL("Winning");
+        }
+    }
+}
