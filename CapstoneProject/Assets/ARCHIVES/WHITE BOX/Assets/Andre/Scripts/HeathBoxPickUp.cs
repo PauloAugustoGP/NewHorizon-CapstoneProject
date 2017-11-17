6f66d31fa071d6166cb1f946fe31c7d8a44913c1
MSI\drema <Dremand@hotmail.com>
2017-11-17 10:59:15 -0500

    Working on WorldSpaceText scene in Andre folder to get healthbox to say you can not pick up any more health if you have full health, needs work to implement things.

---
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Andre/Scripts/HeathBoxPickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class HeathBoxPickUp : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.GetComponent<CharacterBehaviour>())
+        {
+            c.GetComponent<CharacterBehaviour>().Heal(20);
+            Destroy(gameObject);
+        }
+    }
+ 
+}
--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Andre/Scripts/ObjectTriggerDel.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Andre/Scripts/ObjectTriggerDel.cs
@@ -6,10 +6,20 @@ public class ObjectTriggerDel : MonoBehaviour {
 
     public GameObject TextToDelete;
     public GameObject TextToSetActive;
+    public GameObject TextToSetActiveIfTrue;
+    public GameObject PlayerThatMightBeTrue;
 
     private void OnTriggerEnter(Collider c)
     {
-        Destroy(TextToDelete);
-        TextToSetActive.gameObject.SetActive(true);
+        if (PlayerThatMightBeTrue == true)//add in get component to Player the might be true to get the bool that is or is not true
+        { 
+            TextToDelete.gameObject.SetActive(false);
+            TextToSetActiveIfTrue.gameObject.SetActive(true);
+        }
+        else
+        {
+            Destroy(TextToDelete);
+            TextToSetActive.gameObject.SetActive(true);
+        }
     }
 }
