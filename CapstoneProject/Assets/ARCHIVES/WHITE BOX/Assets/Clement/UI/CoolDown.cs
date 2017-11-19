251f021e3a768886186b01a9c90cc050131578da
Clement Abel <clementan25@gmail.com>
2017-11-18 22:26:57 -0500

    Added countdown feature

--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Clement/UI/CoolDown.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Clement/UI/CoolDown.cs
@@ -6,20 +6,38 @@ using UnityEngine.UI;
 public class CoolDown : MonoBehaviour {
 
     public Image coolDown;
-    public float timeRemaining;
+    public bool useScript;
+    public float waitTime;
+    public bool isCoolingDown;
     public Projectile_PlayerScript projectilePlayerScript;
     // Use this for initialization
     void Start () {
-        //projectilePlayerScript = GameObject.FindObjectOfType<Projectile_PlayerScript>();
-        coolDown.fillAmount = 0;
+        if (!projectilePlayerScript) {
+            // first gameobject with this script will be used.
+            projectilePlayerScript = GameObject.FindObjectOfType<Projectile_PlayerScript>();
+            // uses the gameobject that this script is on.
+            //projectilePlayerScript = GetComponent<Projectile_PlayerScript>();
+            waitTime = projectilePlayerScript.coolDownTime;
+            if (!projectilePlayerScript) {
+                Debug.Log("Could not find a script to use. Falling back to default method.");
+                // fallback to use the values in this file as the cooldown;
+                useScript = false;
+            }
+        }
+        if (waitTime <= 0) {
+            waitTime = 2f;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //timeRemaining = projectilePlayerScript.coolDownTime;
-    }
-
-    public void updateProgressBar() {
-        coolDown.fillAmount = timeRemaining;
+        //if (Input.GetKeyDown(KeyCode.X)) {
+        
+        if (useScript) {
+            waitTime = projectilePlayerScript.coolDownTime;
+        }
+        if (isCoolingDown) {
+            coolDown.fillAmount -= 1f / waitTime * Time.deltaTime;
+        }
     }
 }
