1209aec300f09eca2550c71db1efa442b549d5c8
tiffin027 <tiffin027@hotmail.com>
2017-11-20 08:21:57 -0500

    -Changed Projectile to LEFT MOUSE BUTTON and Melee to SPACEBAR. Updated Switch Button prefab.

--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Dan's Folder/Melee.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Dan's Folder/Melee.cs
@@ -46,7 +46,7 @@ public class Melee : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.GetKeyDown(KeyCode.Mouse0))
+		if(Input.GetKeyDown(KeyCode.Space))
 		{
 			StartCoroutine(Attack());
 		}
