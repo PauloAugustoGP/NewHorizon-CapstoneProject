b723fa5cea52e4d6725bd3945285160a1183fd35
MSI\drema <Dremand@hotmail.com>
2017-11-24 09:19:19 -0500

    Prefab of healthpack, working on text above player.

---
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Andre/Scripts/TextAbovePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAbovePlayer : MonoBehaviour {
+
+    public GameObject PlayerTextCanvasText;
+
+    private void Start()
+    {
+        CallText();
+    }
+
+    void CallText()
+    {
+        GameObject temp = Instantiate(PlayerTextCanvasText) as GameObject;
+        RectTransform tempRect = temp.GetComponent<RectTransform>();
+        temp.transform.SetParent(transform.FindChild("PlayerTextCanvas"));
+        tempRect.transform.localPosition = PlayerTextCanvasText.transform.localPosition;
+        tempRect.transform.localScale = PlayerTextCanvasText.transform.localScale;
+        temp.transform.localRotation = PlayerTextCanvasText.transform.localRotation;
+    }
+}
