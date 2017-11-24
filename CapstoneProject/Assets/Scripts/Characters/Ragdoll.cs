081b73fba567ddf59c8a06fa43150b866482eece
Ryck Griffiths <ryckgriff@gmail.com>
2017-11-24 11:02:35 -0500

    Player NOW NEEDS RAGDOLL | Ragdoll still in testing CHECK OFF INVINCIBILITY and leave UseRagdoll UNCHECKED! Cheers

---
+++ CapstoneProject/Assets/Scripts/Characters/Ragdoll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour {
+
+    [SerializeField]
+    Rigidbody[] rigidbodies;
+
+    void Start()
+    {
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+    }
+
+    public Rigidbody[] Bodies
+    {
+        get { return rigidbodies; }
+    }
+}
