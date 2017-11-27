7db62e6d7581dafe05c43a7f446b4f357a085721
Brian Serralheiro <brian.serralheiro@gmail.com>
2017-11-27 09:24:45 -0500

    SwitchButton(Elevator or Door)

--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/switchButton.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/switchButton.cs
@@ -4,36 +4,57 @@ using UnityEngine;
 
 public class SwitchButton : MonoBehaviour
 {
-    [Tooltip("Assign the door you want to open")]
+    [Tooltip("Assign the object you want to control")]
     [SerializeField]
-    private GameObject door;
+    private GameObject Interactable;
     [Tooltip("Script inside of the assigned Door, calls the toggleDoorState function")]
     private Door actualDoor;
+    private Elevator actualElevator;
     [Tooltip("Shows if the door is triggered or not")]
     [SerializeField]
     private bool _switchTriggered;
-
+    [Tooltip("renderer that is being turned into green, after the switch is triggered")]
     MeshRenderer buttonMesh;
 
     void Start()
     {
         buttonMesh = GetComponent<MeshRenderer>();
-        actualDoor = door.GetComponentInChildren<Door>();
+        actualDoor = Interactable.GetComponentInChildren<Door>();
+        actualElevator = Interactable.GetComponent<Elevator>();
     }
     private void OnTriggerEnter(Collider other)
     {   
-        if (other.gameObject.tag == "Projectile")
+        //Verifies if the projectile that hitted and if the button is not activated
+        if (other.gameObject.tag == "Projectile" && !switchTriggered)
         {
-            buttonMesh.material.SetColor("_Color", Color.green);
-            switchTriggered = true;
-            actualDoor.toggleDoorState();
-        }
-            
+            if (actualDoor)
+            {
+                switchTriggered = true;
+                actualDoor.toggleDoorState();
+            }
+            else if (actualElevator)
+            {
+                switchTriggered = true;
+                //Call the elevatortriggered function and gives a reference to the button
+                actualElevator.ElevatorTriggered(this);
+            }
+        }    
     }
     public bool switchTriggered
     {
         get { return _switchTriggered; }
-        set { _switchTriggered = value; }
+        set
+        {
+            _switchTriggered = value;
+            if (_switchTriggered)
+            {
+                buttonMesh.material.SetColor("_Color", Color.green);
+            }
+            else
+            {
+                buttonMesh.material.SetColor("_Color", Color.red);
+            }
+        }
     }
    
 
