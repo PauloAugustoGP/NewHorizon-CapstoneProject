9531ce29b7d2373c528edd32493c4ea908da3f9f
PauloAugusto <pauloaugustogp@gmail.com>
2017-12-01 10:07:25 -0500

    Pause and hud on level 1

--- CapstoneProject/Assets/Scripts/Characters/Player/Melee/Melee.cs
+++ CapstoneProject/Assets/Scripts/Characters/Player/Melee/Melee.cs
@@ -46,7 +46,7 @@ public class Melee : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(Input.GetKeyDown(KeyCode.Mouse1))
 		{
 			StartCoroutine(Attack());
 		}
