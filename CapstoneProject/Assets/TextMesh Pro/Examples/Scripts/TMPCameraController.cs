efc14718cdfed8a3b8cdcd3d6339cefaf473bec6
MSI\drema <Dremand@hotmail.com>
2017-12-01 09:30:36 -0500

    Text Mesh Pro changed CameraController script name to TMPCameraController

--- CapstoneProject/Assets/TextMesh Pro/Examples/Scripts/TMPCameraController.cs
+++ CapstoneProject/Assets/TextMesh Pro/Examples/Scripts/TMPCameraController.cs
@@ -5,7 +5,7 @@ using System.Collections;
 namespace TMPro.Examples
 {
     
-    public class CameraController : MonoBehaviour
+    public class TMPCameraController : MonoBehaviour
     {
         public enum CameraModes { Follow, Isometric, Free }
 
