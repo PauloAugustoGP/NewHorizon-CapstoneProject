fedbbd467aa8e7ef29a3629566181cc86ebdf59e
Brian Serralheiro <brian.serralheiro@gmail.com>
2017-12-15 09:45:42 -0500

    1 one the elevator functionalities working.

--- CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/Elevator.cs
+++ CapstoneProject/Assets/ARCHIVES/WHITE BOX/Assets/Brian Folder/Script/Elevator.cs
@@ -11,12 +11,13 @@ public class Elevator : MonoBehaviour
     private float ElevatorDown;
     [SerializeField]
     private float direction;
+    private Transform Player;
+
 
 	// Use this for initialization
 	void Start ()
     {
         ElevatorDown = transform.position.y;
-        
     }
 	
 	// Update is called once per frame
@@ -25,6 +26,11 @@ public class Elevator : MonoBehaviour
         if (ElevatorON)
         {
             transform.Translate(0, direction * Time.deltaTime, 0);
+            //If the player is under elevator's height, the elevator will go to the lowest position.
+            if(direction > 0 && transform.position.y > Player.position.y)
+            {
+                direction *= -1;
+            }
             if(transform.position.y > ElevatorUP)
             {
                 //elevator arrived at the top
@@ -56,5 +62,13 @@ public class Elevator : MonoBehaviour
             //Forcing direction to be negative number
             direction = -Mathf.Abs(direction);
         }  
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.CompareTag("Player"))
+        {
+            Player = c.transform;
+        }
     }
 }
